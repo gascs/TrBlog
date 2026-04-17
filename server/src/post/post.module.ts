c58cba1400987db016cd60f3f28c182b10f58f7b
gascs <gascs@users.noreply.github.com>
2026-04-17 05:53:18 +0000

    feat: comprehensive optimization
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- server/src/post/post.module.ts
+++ server/src/post/post.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PostService } from './post.service';
 import { PostController } from './post.controller';
 import { PrismaModule } from '../prisma/prisma.module';
+import { CommonModule } from '../common/common.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, CommonModule],
   controllers: [PostController],
   providers: [PostService],
   exports: [PostService],
